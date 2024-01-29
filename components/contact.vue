ca33a0fb7c9e6813bc48352f5032b54a90c7d985
Kongkong <philasting@163.com>
2024-01-29 15:20:56 +0000

    Update contact

--- components/contact.vue
+++ components/contact.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="container flex px-3 pt-10 md:pt-16">
+  <div class="contact container flex px-3 pt-10 md:pt-16">
     <div class="lg:w-2/12"></div>
     <div class="lg:w-10/12 flex flex-wrap items-center -mx-3 contact__form__wrapper p-3 lg:p-14 relative z-10">
       <div class="w-full lg:w-7/12 px-10 md:px-3">
         <div class="content__form__area">
           <h3 class="mb-10 leading-tight text-[25px] md:text-[48px]">{{ title }}</h3>
           <div class="contact__form__inner">
-            <UInput v-model="email" class="mb-5" color="transparent" variant="outline" placeholder="Email" />
-            <USelect v-model="selectQuestion" class="mb-5" color="transparent" variant="outline" :options="question" />
-            <UInput v-model="desc" color="transparent" variant="outline" placeholder="Describe your questions here" />
-
-            <!-- Submit button -->
-            <button @click="handleSubmit"
-              class="bg-[#FA8C16] min-w-[192px] text-white p-3 mt-[32px]  rounded-[6px] hover:bg-[#F97316] transition duration-300">
-              Submit
+            <div class="text">{{ content }}</div>
+            <button class="bg-[#FA8C16] min-w-[192px] text-white p-3 mt-[32px]  rounded-[6px] hover:bg-[#F97316] transition duration-300">
+              <a href="https://forms.gle/TTxnVhnEyNEkyXUAA" target="_blank" rel="noopener noreferrer">{{ btnText }}</a>
             </button>
           </div>
 
@@ -28,18 +23,19 @@
 </template>
 
 <script setup>
-const title = "Get in touch with the team";
-const question = reactive([
-  'Development Questions',
-  'Marketing Questions',
-  'Looking for Cooperation',
-]);
-const email = ref('');
-const desc = ref('');
-const selectQuestion = reactive(question[0]);
+const title = "Start Your Crust Journey Now!";
+const content = "Ready to dive into the world of decentralized solutions with Crust? Click below to connect with our experts and explore the possibilities!";
+const btnText = "Connect with Crust Team";
+
 const handleSubmit = () => {
   console.log('Form submitted');
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.contact {
+  .text {
+    color: rgba(255, 255, 255, 0.60);
+  }
+}
+</style>
